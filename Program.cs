fa36449997bb08594345a25f7f5f1cb045dd4118
Pheeraphat Prisan <77052746+newpheeraphat@users.noreply.github.com>
2024-03-28 02:40:47 +0700

    feat: Finish data model

--- Program.cs
+++ Program.cs
@@ -1,84 +1,13 @@
-using GameStore.Dtos;
+using GameStore.Data;
+using GameStore.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString = builder.Configuration.GetConnectionString("GameStore");
+builder.Services.AddSqlite<GameStoreContext>(connString);
+
 var app = builder.Build();
 
-const string GetGameEndPointName = "GetGame"; 
-
-List<GameDto> games = [
-  new (
-    1,
-    "Street Fighter",
-    "Fighting",
-    19.99M,
-    new DateOnly(1997, 11, 15)
-  ),
-  new (
-    2,
-    "Final Fantasy XIV",
-    "Roleplaying",
-    59.99M,
-    new DateOnly(2010, 9, 30)
-  ),
-  new (
-    3,
-    "FIFA 23",
-    "Sports",
-    69.99M,
-    new DateOnly(2022, 9, 26)
-  )
-];
-
-// GET /games
-app.MapGet("games", () => games);
-
-// GET /games/1
-app.MapGet("games/{id}", (int id) => {
-  GameDto? game = games.Find(game => game.Id == id);
-
-  return game is null ? Results.NotFound() : Results.Ok(game);
-})
-.WithName(GetGameEndPointName);
-
-// POST /games
-app.MapPost("games", (CreateGameDto newGame) => {
-
-  GameDto game = new (
-    games.Count + 1,
-    newGame.Name,
-    newGame.Genre,
-    newGame.Price,
-    newGame.ReleaseDate
-  );
-
-  games.Add(game);
-
-  return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
-});
-
-// PUT /games/1
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) => {
-  int index = games.FindIndex(game => game.Id == id);
-
-  if (index == -1) {
-    return Results.NotFound();
-  }
-
-  games[index] = new GameDto(
-    id,
-    updatedGame.Name,
-    updatedGame.Genre,
-    updatedGame.Price,
-    updatedGame.ReleaseDate
-  );
-
-  return Results.NoContent();
-});
-
-// DELETE /games/1
-app.MapDelete("games/{id}", (int id) => {
-  games.RemoveAll(game => game.Id == id);
-  return Results.NoContent();
-});
+GamesEndpoints.MapGamesEndpoints(app);
 
 app.Run();
