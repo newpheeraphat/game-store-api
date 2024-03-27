bcb1e60425895f27961b2666e542984b0b4bacbc
Pheeraphat Prisan <77052746+newpheeraphat@users.noreply.github.com>
2024-03-27 20:31:52 +0700

    feat: Add the CRUD endpoint

--- obj/Debug/net7.0/GameStore.AssemblyInfo.cs
+++ obj/Debug/net7.0/GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f49d8ecca291f7087429b9c8e123770c243c30")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
